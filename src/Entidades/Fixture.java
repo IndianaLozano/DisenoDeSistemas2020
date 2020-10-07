dc7a1565c4fea722f8d505e000d27538f70ca0e1
IndianaLozano <IndianaLozano>
2020-10-07 13:28:19 -0300

    Cambios simples.
    Agregado de clases Localidad, Pais, Provincia.
    Agregado de constructores en clase Fixture.

--- src/Entidades/Fixture.java
+++ src/Entidades/Fixture.java
@@ -1,5 +1,32 @@
 package Entidades;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Fixture {
 
+	public int idFixture;
+	public Date fecha;
+	
+	//Creacion del constructor
+	public Fixture() {
+		
+	}
+	
+	//Creacion del constructor con parametros
+	public Fixture (int idFixture, Date fecha) {
+		this.idFixture = idFixture;
+		this.fecha = fecha;
+		
+	}
+	
+	//BDD envia datos todos juntos en un String. Separo y convierto los datos que no son String en su respectivo tipo.
+	public Fixture (String datos) throws ParseException {
+		String[] atributo = datos.split("\t");
+		this.idFixture = Integer.parseInt(atributo[0]);
+		Date fecha = new SimpleDateFormat("dd/MM/yyyy").parse(atributo[1]);  
+		this.fecha = fecha;
+	}
+	
 }
